8485a898039775fa05461d0fc616e778cd3cd882
asuhantsev <imhnugxm138@hotmail.com>
2024-12-27 19:26:13 +0300

    fix: update deployment configuration to match repository name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pong-mp/',
+  base: '/pong/',
   plugins: [react()],
   server: {
     hmr: {
